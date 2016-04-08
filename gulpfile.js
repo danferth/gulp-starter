2037d10d0b2f2e008f93f7cf5c094b172ce59375
Dan Klotz <danferth@gmail.com>
2016-04-08 00:49:15 +0000

    fixed scss and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var Promise = require('es6-promise').Promise;
 //variables
 var 
-    scss_src        = "assets/dev/scss",
+    dev_src         = "assets/dev",
     css_dest        = "assets/css",
-    js_src          = "assets/dev/js",
     js_dest         = "assets/js",
     gulp            = require('gulp'),
     sass            = require('gulp-sass'),
     postcss         = require('gulp-postcss'),
     autoprefixer    = require('autoprefixer'),
     csswring        = require('csswring'),
-    sourcemaps      = require('gulp-sourcemaps');
+    sourcemaps      = require('gulp-sourcemaps'),
+    concat          = require('gulp-concat'),
+    uglify          = require('gulp-uglify');
     
 
 //default task
@@ -21,7 +22,7 @@ gulp.task('default',['watch']);
 gulp.task('build-css',function(){
     var processors = [autoprefixer({browsers:['last 2 version']}),csswring];
     var build_processors = [csswring];
-    return gulp.src(scss_src + '/global.scss')
+    return gulp.src(dev_src + '/scss/global.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(postcss(processors))
@@ -29,8 +30,16 @@ gulp.task('build-css',function(){
     .pipe(gulp.dest(css_dest));
 });
 
+gulp.task('build-js', function(){
+    return gulp.src([dev_src+'/lib/**',dev_src+'/js/**'])
+        .pipe(concat('site.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(js_dest));
+});
+
 
 //watch
 gulp.task('watch',function(){
-    gulp.watch(scss_src + '/**', ['build-css']);
+    gulp.watch(dev_src + '/scss/**', ['build-css']);
 });
