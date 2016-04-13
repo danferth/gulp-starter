0509eeafc89704587b70bd1e0abdf56c74beb26d
Dan Klotz <danferth@gmail.com>
2016-04-13 22:22:41 +0000

    have gulp changed working and on the css and js task

--- gulpfile.js
+++ gulpfile.js
@@ -46,72 +46,72 @@ gulp.task('default',['watch']);
 //==========help===================================================================
 //=================================================================================
 gulp.task('help', function(){
-    console.log("=============================================================".bold.green);
-    console.log("clean           = delete contents of build folder".red);
-    console.log("css             = sourcemaps | sass | prefix | minimize | filesize".cyan);
-    console.log("js              = concat | jshint | filesize".yellow);
-    console.log("js --production = concat | sourcemaps | minimize | filesize".yellow);
-    console.log("images          = optimize images and save to build dir".grey);
-    console.log("watch (default) = build-css && js".bold.green);
-    console.log("=============================================================".bold.yellow);
+  console.log("=============================================================".bold.green);
+  console.log("clean           = delete contents of build folder".red);
+  console.log("css             = sourcemaps | sass | prefix | minimize | filesize".cyan);
+  console.log("js              = concat | jshint | filesize".yellow);
+  console.log("js --production = concat | sourcemaps | minimize | filesize".yellow);
+  console.log("images          = optimize images and save to build dir".grey);
+  console.log("watch (default) = build-css && js".bold.green);
+  console.log("=============================================================".bold.yellow);
 });
 
 //clean
 gulp.task('clean', function(){
-    return del([
-        dest + '/**/*'
-        ]);
+   return del([
+      dest + '/**/*'
+    ]);
 });
 
 //=========stylesheet====================================================================
 //sourcemaps | sass | prefix | minimize | filesize
 gulp.task('css',function(){
-    var processors = [autoprefixer({browsers:['last 2 version']}),csswring];
-    return gulp.src(css_src)
-    .pipe(changed(css_dest))
-    .pipe(sasslint())
-    .pipe(sasslint.format())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(postcss(processors))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(css_dest))
-    .pipe(filesize());
+  var processors = [autoprefixer({browsers:['last 2 version']}),csswring];
+  return gulp.src(css_src)
+  .pipe(changed(css_dest))
+  .pipe(sasslint())
+  .pipe(sasslint.format())
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(postcss(processors))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(css_dest))
+  .pipe(filesize());
 });
 
 //=======javascript=================================================================
 //concat | jshint | filesize
 //(--production) concat | sourcemaps | minimize | filesize
 gulp.task('js', function(){
-    return gulp.src([js_lib_src +'/**',js_src + '/**'])
-        .pipe(concat(js_file))
-        
-        .pipe(gulpif(argv.production, filesize()))
-        .pipe(gulpif(argv.production, sourcemaps.init()))
-        .pipe(gulpif(argv.production, uglify()))
-        .pipe(gulpif(argv.production, sourcemaps.write()))
-
-        .pipe(gulpif(!argv.production, jshint.reporter('jshint-stylish')))
-        
-        .pipe(gulp.dest(js_dest))
-        .pipe(filesize());
+  return gulp.src([js_lib_src +'/**',js_src + '/**'])
+  .pipe(changed(js_dest))
+  .pipe(concat(js_file))
+  
+  .pipe(gulpif(argv.production, filesize()))
+  .pipe(gulpif(argv.production, sourcemaps.init()))
+  .pipe(gulpif(argv.production, uglify()))
+  .pipe(gulpif(argv.production, sourcemaps.write()))
+  .pipe(gulpif(!argv.production, jshint.reporter('jshint-stylish')))
+  
+  .pipe(gulp.dest(js_dest))
+  .pipe(filesize());
 });
 
 //========images=================================================================
 gulp.task('image', function(){
-    return gulp.src(image_src + '/**')
-    .pipe(imagemin({
-        progressive: true,
-        svgPlugins: [{removeViewBox: false}],
-        use: [pngquant()]
-    }))
-    .pipe(gulp.dest(image_dest));
+  return gulp.src(image_src + '/**')
+  .pipe(imagemin({
+      progressive: true,
+      svgPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+  }))
+  .pipe(gulp.dest(image_dest));
 });
 
 //========watch===================================================================
 gulp.task('watch',function(){
-    gulp.watch(css_watch, ['css']);
-    gulp.watch([js_lib_src + '/**/**', js_src + '/**/**'],['js']);
+  gulp.watch(css_watch, ['css']);
+  gulp.watch([js_lib_src + '/**/**', js_src + '/**/**'],['js']);
 });
 
 
@@ -121,11 +121,11 @@ gulp.task('watch',function(){
 //======TEST=========================================================
 
 gulp.task('testme', function(){
-    console.log("====| this is a test |====".bold.green);
+  console.log("====| this is a test |====".bold.green);
 
-    if(argv.production){
-        console.log("let's do this");
-    }
+  if(argv.production){
+    console.log("let's do this");
+  }
 
-    console.log("====| test complete! Hope it worked out :o) |====".bold.red);
+  console.log("====| test complete! Hope it worked out :o) |====".bold.red);
 });
