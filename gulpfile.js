4582445a5e59b3b66503a069e58211600734ba95
Dan Klotz <danferth@gmail.com>
2016-04-13 17:49:17 +0000

    added variables for each src and destination for easy configuring

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var Promise = require('es6-promise').Promise;
 //variables
 var 
-    src             = "assets/dev",
-    dest            = "assets/build",
     gulp            = require('gulp'),
     sass            = require('gulp-sass'),
     postcss         = require('gulp-postcss'),
@@ -17,61 +15,35 @@ var
     imagemin        = require('gulp-imagemin'),
     pngquant        = require('imagemin-pngquant'),
     del             = require('del');
+
+//options
+var src     = "assets/dev",
+    dest    = "assets/build",
     
+    css_file    = "global",
+    css_src = src + "/scss/" + css_file + ".scss",
+    css_dest    = dest + "/css",
+    
+    js_file     = "site.js",
+    js_lib_src      = src + "/lib",
+    js_src      = src + "/js",
+    js_dest         = dest + "/js",
+    
+    image_src       = src + "/img",
+    image_dest      = dest + "/images";
 
 //default task
-
 gulp.task('default',['watch']);
 
-//stylesheet
-//sourcemaps | sass | prefix | minimize | filesize
-gulp.task('build-css',function(){
-    var processors = [autoprefixer({browsers:['last 2 version']}),csswring];
-    var build_processors = [csswring];
-    return gulp.src(src + '/scss/global.scss')
-    .pipe(sasslint())
-    .pipe(sasslint.format())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(postcss(processors))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(dest + '/css'))
-    .pipe(filesize());
-});
 
-//javascript
-
-//concat | jshint | filesize
-gulp.task('js', function(){
-    return gulp.src([src+'/lib/**',src+'/js/**'])
-        .pipe(concat('site.js'))
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(gulp.dest(dest + '/js'))
-        .pipe(filesize())
-});
-
-//concat | sourcemaps | minimize | filesize
-gulp.task('build-js', function(){
-    return gulp.src([src+'/lib/**',src+'/js/**'])
-        .pipe(concat('site.js'))
-        .pipe(filesize())
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(dest + '/js'))
-        .pipe(filesize());
-});
-
-//images
-gulp.task('image', function(){
-    return gulp.src(src + '/img/**')
-    .pipe(imagemin({
-        progressive: true,
-        svgPlugins: [{removeViewBox: false}],
-        use: [pngquant()]
-    }))
-    .pipe(gulp.dest(dest + "/images"))
+//help
+gulp.task('help', function(){
+    console.log("clean = delete contents of build folder");
+    console.log("build-css = sourcemaps | sass | prefix | minimize | filesize");
+    console.log("js = concat | jshint | filesize");
+    console.log("build-js = concat | sourcemaps | minimize | filesize");
+    console.log("images = optimize images and save to build dir");
+    console.log("watch = build-css && js");
 });
 
 //clean
@@ -79,6 +51,57 @@ gulp.task('clean', function(){
     return del([
         dest + '/**/*'
         ]);
+});
+
+//stylesheet
+//sourcemaps | sass | prefix | minimize | filesize
+gulp.task('build-css',function(){
+    var processors = [autoprefixer({browsers:['last 2 version']}),csswring];
+    var build_processors = [csswring];
+    return gulp.src(css_src)
+    .pipe(sasslint())
+    .pipe(sasslint.format())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(css_dest))
+    .pipe(filesize());
+});
+
+//javascript
+
+//concat | jshint | filesize
+gulp.task('js', function(){
+    return gulp.src([js_lib_src +'/**',js_src + '/**'])
+        .pipe(concat(js_file))
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(gulp.dest(js_dest))
+        .pipe(filesize())
+});
+
+//concat | sourcemaps | minimize | filesize
+gulp.task('build-js', function(){
+    return gulp.src([js_lib_src +'/**',js_src + '/**'])
+        .pipe(concat(js_file))
+        .pipe(filesize())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(js_dest))
+        .pipe(filesize());
+});
+
+//images
+gulp.task('image', function(){
+    return gulp.src(image_src + '/**')
+    .pipe(imagemin({
+        progressive: true,
+        svgPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+    }))
+    .pipe(gulp.dest(image_dest))
 });
 
 
